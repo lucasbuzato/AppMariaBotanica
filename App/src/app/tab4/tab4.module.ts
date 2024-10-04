a4f71508cbca850ffe56f59a5b0294cddf155298
Laboratório FIAP <labsfiap@fiap.com.br>
2024-10-04 09:59:50 -0300

    tab

--- App/src/app/tab4/tab4.module.ts
+++ App/src/app/tab4/tab4.module.ts
@@ -2,10 +2,9 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
+import { Tab4Page } from './tab4.page';
 
-
-import { Tab2PageRoutingModule } from './tab2-routing.module';
+import { Tab4PageRoutingModule } from './tab4-routing.module';
 
 @NgModule({
   imports: [
@@ -13,8 +12,9 @@ import { Tab2PageRoutingModule } from './tab2-routing.module';
     CommonModule,
     FormsModule,
 
-    Tab2PageRoutingModule
+    Tab4PageRoutingModule
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab4Page]
 })
-export class Tab2PageModule {}
+export class Tab4PageModule {}
+
